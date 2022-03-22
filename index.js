827e732ec3213e3a7b8eb1f52470616e0abfe4f4
AlexendrRobson <102164498+AlexendrRobson@users.noreply.github.com>
2022-03-22 16:06:44 +0200

    Create index.js
    text

---
+++ index.js
@@ -0,0 +1,22 @@
+Rites) (ac Cia ee
+this.letter = letter;
+Ca ea ed
+this.used = used;
+Rc ee ey
+Cao eso
+
+LesCUMsUe (ota o nee
+
+Sea @e Cre eee a
+Cy te ates ed ce ec re a
+
+none
+Peano tra oe tae enue ac me
+yaaa
+
+else if (block.letter == tree[preminIndex].letter) {
+yaaa Ce a
+
+ene
+FindCode(tree[block.father]);
+ya ee RD re eee a
